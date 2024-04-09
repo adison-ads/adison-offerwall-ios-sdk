5cd2c379913848d06802f3a02a5d380d8dbb09b5
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-04-09 20:28:13 +0900

    3.5.0-beta3 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 let repoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download"
-let artifactVersion = "3.5.0-beta2"
+let artifactVersion = "3.5.0-beta3"
 let artifactName = "AdisonOfferwallSDK.zip"
-let checksum = "09353c9826c91c4d2459a8e77a25e481c43682dcfd084131570930c7f36e3022"
+let checksum = "2eacb67861c694a4e6f417c5390b158712d70e898e9955aa6464a2f1dc2819db"
 
 // MARK: - Do not change.
 let package = Package(
