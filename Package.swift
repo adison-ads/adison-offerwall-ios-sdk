a0c2a98a8a9b2ed5e8a500f8f149118a4f4253ce
Sangkyoon Nam <nam.sangkyoon@nbt.com>
2024-05-27 21:00:27 +0900

    3.5.6 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 let repoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download"
-let artifactVersion = "3.5.4"
+let artifactVersion = "3.5.6"
 let artifactName = "AdisonOfferwallSDK.zip"
-let checksum = "706af278073888e3cd3998e8df3bd82d1dca774077694faa6c20922e04baf85d"
+let checksum = "bb396550c330edd114dc5cc6956050e2696180f4fbdbcef775cd6614a7910b7d"
 
 // MARK: - Do not change.
 let package = Package(
