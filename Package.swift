9d3ac3f7dc4baae9b3cab873ddaeaffd07490194
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2025-03-04 14:26:33 +0900

    3.11.0 릴리즈
    - 안드로이드와 리스트 타입 버전 통일

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 let repoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download"
-let artifactVersion = "3.9.8"
+let artifactVersion = "3.11.0"
 let artifactName = "AdisonOfferwallSDK.zip"
-let checksum = "84a1a10124aeab168855cab7d3b2f05c88259c4acd7ac7fec5602c3379533b90"
+let checksum = "5dab10dd655075bdf9b5c87d1970622f6cb6f4d5fd46e69d4a2c93486836d635"
 
 let lumberjackVersion = "0.9.10"
 
