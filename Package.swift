9f52d96ce6f80358da8455d60d83f9c55565c2a8
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2025-04-19 19:53:39 +0900

    패키지 파일 수정

--- Package.swift
+++ Package.swift
@@ -1,44 +1,24 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.10
 
 import PackageDescription
 
-let repoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download"
-let artifactVersion = "3.11.0"
-let artifactName = "AdisonOfferwallSDK.zip"
-let checksum = "5dab10dd655075bdf9b5c87d1970622f6cb6f4d5fd46e69d4a2c93486836d635"
-
-let lumberjackVersion = "0.9.10"
-
-// MARK: - Do not change.
 let package = Package(
     name: "AdisonOfferwallSDK",
+    platforms: [
+    ],
     products: [
         .library(
             name: "AdisonOfferwallSDK",
             targets: [
                 "AdisonOfferwallSDK",
-                "AdisonOfferwallSDKTarget"
             ]
-        )
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/adison-ads/adison-offerwall-lumberjack",
-            exact: Version(stringLiteral: lumberjackVersion)
         )
     ],
     targets: [
         .binaryTarget(
             name: "AdisonOfferwallSDK",
-            url: repoURL + "/" + artifactVersion + "/" + artifactName,
-            checksum: checksum
-        ),
-        .target(
-            name: "AdisonOfferwallSDKTarget",
-            dependencies: [
-                .product(name: "AdisonOfferwallLumberjack", package: "adison-offerwall-lumberjack")
-            ],
-            path: "Sources"
+            url: "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download/3.11.0/AdisonOfferwallSDK.zip",
+            checksum: ""
         )
     ]
 )
