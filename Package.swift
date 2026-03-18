36ef6df84dfb89ab42e1eef4b3858468149d94ef
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2026-03-18 16:09:51 +0900

    4.7.0 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 let repoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk/releases/download"
-let artifactVersion = "4.6.0"
+let artifactVersion = "4.7.0"
 let artifactName = "AdisonOfferwallSDK.zip"
-let checksum = "cf4be506d6f9409db6e7e0419a9fd75225c0d330d9569370163677dd768613a2"
+let checksum = "c7d3cc2b1a0cc3a0720539606cd43b68faa34bd7f48eeea4af689880f4423b5a"
 
 // MARK: - Do not change.
 let package = Package(
@@ -20,8 +20,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-global-core", exact: "0.27.2"),
-        .package(url: "https://github.com/adison-ads/adison-offerwall-lumberjack", exact: "0.9.10")
+        .package(url: "https://github.com/adison-ads/adison-offerwall-global-core", exact: "1.2.0"),
+        .package(url: "https://github.com/adison-ads/adison-offerwall-lumberjack", exact: "0.9.14")
     ],
     targets: [
         .binaryTarget(
